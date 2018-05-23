964058f918ddf58f34b1ac231d4c00b9bcec89e7
Daniel Brumazzi <brumazzi_daniel@yahoo.com.br>
2018-05-23 00:30:26 -0400

    i3 config 0.2.4

--- date.sh
+++ date.sh
@@ -11,14 +11,14 @@ fi
 HOUR=$(date '+%H')
 SYMB=""
 CLOCK_LIST_00="🕛;🕐;🕑;🕒;🕓;🕔;🕕;🕖;🕗;🕘;🕙;🕚"
-CLOCK_LIST_30=";🕧;🕜;🕝;🕞;🕟;🕠;🕡;🕢;🕣;🕤;🕥;🕦"
+CLOCK_LIST_30="🕧;🕜;🕝;🕞;🕟;🕠;🕡;🕢;🕣;🕤;🕥;🕦"
 IFS=';'
 CLOCKS00=($CLOCK_LIST_00)
 CLOCKS30=($CLOCK_LIST_30)
 
-let H=$HOUR % 12
-let M=$(date '+%M')
-if [ "$M" -l 30 ]; then
+let H="$HOUR % 12"
+M=$(date '+%M')
+if [ "$M" -le 30 ]; then
 	CLOCK="${CLOCKS00[$H]}"
 else
 	CLOCK="${CLOCKS30[$H]}"
@@ -30,4 +30,4 @@ else
 	SYMB="<span color='#727FeF'>☪</span>"
 fi
 
-echo -e "<b>\U1f4c5</b> $(date '+%a %d %b')<b> $CLOCK </b>$(date '+%H:%M') <b>$SYMB</b>"
+echo -e "<b>\U1f4c5</b> $(date '+%a %d %b') $CLOCK $(date '+%H:%M') <b>$SYMB</b>"
