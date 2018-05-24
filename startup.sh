bacadc722044c00394169d1496ed6fb6728291b0
Daniel Brumazzi <brumazzi_daniel@yahoo.com.br>
2018-05-24 14:01:24 -0400

    i3 config 0.2.4

--- startup.sh
+++ startup.sh
@@ -1,11 +1,15 @@
 #!/bin/sh
 
-PROCS="conky" #"nm-applet"
+PROCS="conky" # nm-applet"
 
 for proc in $PROCS; do
-	( [[ "$(pgrep $proc)" ]] && echo $proc up ||
+	( 
+		sleep 1
+		[[ "$(pgrep $proc)" ]] && echo $proc up ||
 		if [ "$proc" == "conky" ]; then
 			$proc -c ~/.config/i3/conky.conf
+		else
+			$proc
 		fi
 	) &
 done
